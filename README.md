d394492729b0bc279536a81d67fb46747b659498
korrio <iamnotkorr@gmail.com>
2021-06-20 22:30:44 +0700

    :rocket: prepare for launch

---
+++ README.md
@@ -0,0 +1,13 @@
+## Contracts
+VonderToken: https://testnet.bkcscan.com/address/0xb167658b19e5BcD3a6106E6C6B73c8c35f1D6bfe/
+MasterChef: https://testnet.bkcscan.com/address/0x45c585A81EDEc7B3F29dE43De5A3bF2265b384Fa/
+
+KUSDT: https://bkcscan.com/address/0x80318CAB3791E49650C8760a61196fFD2D23F6a1/contracts
+
+KUSDC:  https://bkcscan.com/address/0x8b614b636FfDdfFaa261224d88C3Fc919a9634AE/contracts
+
+KDAI: https://bkcscan.com/address/0xc6677E014D7e2F45fB44E8036C014B916C0492a1/contracts
+
+KBTC: https://bkcscan.com/address/0x0330b553823703E673787747D1930a12D7a14c94/contracts
+
+KETH: https://bkcscan.com/address/0xE06B321eF826eaB4D242b1e40d4a51b8dCDF61B2/contracts
---
+++ truffle-config.js
@@ -0,0 +1,82 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const fs = require('fs');
+
+const { infuraProjectId, mainnetBkcPrivateKeys, testnetBkcPrivateKeys, mainnetPrivateKeys, privateKeys, etherApiKey, bscApiKey } = JSON.parse(fs.readFileSync('.secret').toString().trim());
+
+const binanceProvider = new HDWalletProvider({
+    privateKeys: privateKeys,
+    providerOrUrl: `https://data-seed-prebsc-1-s1.binance.org:8545`
+});
+
+const bitkubMainnetProvider = new HDWalletProvider({
+    privateKeys: mainnetBkcPrivateKeys,
+    providerOrUrl: `https://rpc.bitkubchain.io`
+});
+
+const bitkubTestnetProvider = new HDWalletProvider({
+    privateKeys: testnetBkcPrivateKeys,
+    providerOrUrl: `https://rpc-testnet.bitkubchain.io`
+});
+
+module.exports = {
+    // Uncommenting the defaults below
+    // provides for an easier quick-start with Ganache.
+    // You can also follow this format for other networks;
+    // see <http://truffleframework.com/docs/advanced/configuration>
+    // for more details on how to specify configuration options!
+    //
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        },
+        test: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        },
+        binanceTestnet: {
+            provider: () => binanceProvider,
+            network_id: '97',
+            gas: 5500000,
+            gasPrice: Web3.utils.toWei('10', 'gwei'),
+            skipDryRun: true,
+        },
+        bkcMainnet: {
+            provider: () => bitkubMainnetProvider,
+            network_id: '96',
+            gas: 5500000,
+            gasPrice: Web3.utils.toWei('50', 'gwei'),
+            skipDryRun: true,
+        },
+        bkcTestnet: {
+            provider: () => bitkubTestnetProvider,
+            network_id: '25925',
+            gas: 5500000,
+            gasPrice: Web3.utils.toWei('50', 'gwei'),
+            skipDryRun: true,
+        },
+    },
+    //
+    compilers: {
+        solc: {
+            version: "0.6.12",
+            settings: { // See the solidity docs for advice about optimization and evmVersion
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                },
+                //  evmVersion: 'byzantium'
+            }
+        }
+    },
+    plugins: [
+        'truffle-plugin-verify'
+    ],
+    api_keys: {
+        etherscan: etherApiKey,
+        bscscan: bscApiKey,
+    }
+};
