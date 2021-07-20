1d843e077df1037ea9b532fe4be85c62243a2c40
vonder.finance <vonder.finance@gmail.com>
2021-07-20 10:18:17 +0700

    clean code and prepare for auditing

--- contracts/VonderToken.sol
+++ contracts/VonderToken.sol
@@ -685,7 +685,7 @@ contract BEP20 is Context, IBEP20, Ownable {
 }
 
 // VonderToken with Governance.
-contract VonderToken is BEP20('VONDER Token', 'VON-T') {
+contract VonderToken is BEP20('VONDER Token', 'VON') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
